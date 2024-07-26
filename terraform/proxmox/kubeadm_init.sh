035bf109ae348e22a9aa1f131693445b06ee036c
charlin <charleskrodrigues@hotmail.com>
2024-07-26 12:47:53 -0300

    chore(kubeadm-init): move apt-update to the beginning

--- terraform/proxmox/kubeadm_init.sh
+++ terraform/proxmox/kubeadm_init.sh
@@ -22,6 +22,7 @@ EOF
 sudo sysctl --system
 
 # Install containerd packages
+sudo apt-get update
 sudo apt-get install -y containerd.io
 # Make containerd config.toml file default
 sudo containerd config default | sudo tee /etc/containerd/config.toml
@@ -45,14 +46,12 @@ sudo usermod -aG docker crodrigues
 ### Installing kubeadm, kubelet and kubectl
 # -> these instructions are for Kubernetes v1.29 <-
 # Update the apt package index and install packages needed to use the Kubernetes apt repository:
-sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 # Download the public signing key for the Kubernetes package repositories. The same signing key is used for all repositories so you can disregard the version in the URL:
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 # Add the appropriate Kubernetes apt repository.
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 # Update the apt package index, install kubelet, kubeadm and kubectl, and pin their version:
-sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 # Enable the kubelet service before running kubeadm:
