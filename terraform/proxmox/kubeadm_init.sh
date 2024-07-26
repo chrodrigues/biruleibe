32bb1d16b1f340327ba46cc814ae7a2c4b4b5535
charlin <charleskrodrigues@hotmail.com>
2024-07-26 12:03:55 -0300

    chore(kubeadm-init): define containerd as container runtime

--- terraform/proxmox/kubeadm_init.sh
+++ terraform/proxmox/kubeadm_init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Installing and Configuring containerd as a Kubernetes Container Runtime (https://www.nocentino.com/posts/2021-12-27-installing-and-configuring-containerd-as-a-kubernetes-container-runtime/#configure-required-modules)
-#### Configure required modules
+#### Installing and Configuring containerd as a Kubernetes Container Runtime (https://www.nocentino.com/posts/2021-12-27-installing-and-configuring-containerd-as-a-kubernetes-container-runtime/#configure-required-modules)
+# Configure required modules
 # First, load two modules in the current running environment and configure them to load on boot.
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 overlay
@@ -11,25 +11,25 @@ EOF
 sudo modprobe overlay
 sudo modprobe br_netfilter
 
-## Configure required sysctl to persist across system reboots
+# Configure required sysctl to persist across system reboots
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
 EOF
 
-### Apply sysctl parameters without rebooting to the current running environment
+# Apply sysctl parameters without rebooting to the current running environment
 sudo sysctl --system
 
-### Install containerd packages
+# Install containerd packages
 sudo apt-get install -y containerd.io
-### Make containerd config.toml file default
+# Make containerd config.toml file default
 sudo containerd config default | sudo tee /etc/containerd/config.toml
 
-### Set the cgroup driver for runc to systemd
+# Set the cgroup driver for runc to systemd
 sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
 
-### Restart containerd with the new configuration
+# Restart containerd with the new configuration
 sudo systemctl restart containerd
 
 #### Disable ipv6
